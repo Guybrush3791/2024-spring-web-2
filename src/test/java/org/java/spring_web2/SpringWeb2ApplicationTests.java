51345fbd27f726f8924ba6fb1a489511efe88c30
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-12 14:49:48 +0200

    init

---
+++ src/test/java/org/java/spring_web2/SpringWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.java.spring_web2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
