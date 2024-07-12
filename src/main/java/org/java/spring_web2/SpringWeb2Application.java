0710e2280fab2e1d5f53280846651ffd29d5c268
Giovanni Cappelletti <tech@methodwolf.com>
2024-07-12 14:52:05 +0200

    update

--- src/main/java/org/java/spring_web2/SpringWeb2Application.java
+++ src/main/java/org/java/spring_web2/SpringWeb2Application.java
@@ -6,6 +6,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringWeb2Application {
 
+	/**
+	 * TODO:
+	 * 
+	 * In nuovo esercizio creare entita' e tabella in database come segue:
+	 * 
+	 * Machine:
+	 * - id : INT
+	 * - name : STRING
+	 * - description : STRING
+	 * - price : INT
+	 * 
+	 * Dopo aver creato entita', repo e service, aggiungere controller per
+	 * l'interazione con l'utente. Definire 2 endpoints per:
+	 * - aggiungere 3 macchine con dati a piacere
+	 * - leggere l'intero contenuto della tabella e ritornarlo come JSON
+	 * 
+	 * Testare che l'intero contenuto della tabella sia accessibile attraverso
+	 * Postman e le chiamate ai due endpoints definiti.
+	 * 
+	 */
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWeb2Application.class, args);
 	}
